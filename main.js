c86a3181b895b3aae5bb70bd5e660122d86c1b7b
Arashi <35284190+cd-7@users.noreply.github.com>
2022-03-16 14:32:34 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 var currentWordleAnswer = "blast";
+var currentWordLetter1 = currentWordleAnswer.charAt(0);
+var currentWordLetter2 = currentWordleAnswer.charAt(1);
+var currentWordLetter3 = currentWordleAnswer.charAt(2);
+var currentWordLetter4 = currentWordleAnswer.charAt(3);
+var currentWordLetter5 = currentWordleAnswer.charAt(4);
 var guess = "hello";
 var guess1 = "";
 var guess2 = "";
@@ -39,29 +44,72 @@ var guess6Letter5 = "";
 
 hideWarning();
 function hideWarning() {
-  document.getElementById("warning__label").style.display =
-    "none";
+  document.getElementById("warning__label").style.display = "none";
 }
 
-function checkGuess() {
+function checkAllGuesses() {
   while (true) {
     guess = document.getElementById("guess__input").value;
-
     if (guess.length == 5) {
-      guess1 = guess;
       break;
     } else {
-      document.getElementById(
-        "warning__label"
-      ).style.display = "block";
+      document.getElementById("warning__label").style.display = "block";
       setTimeout(hideWarning, 5000);
       return;
     }
   }
+  checkGuess1();
+}
+
+function checkGuess1() {
+  if (guess1 != "") {
+    return;
+  }
+  guess1 = guess;
   guess1Letter1 = guess1.charAt(0);
   document.getElementById("guess1--letter1").innerHTML = guess1Letter1;
   guess1Letter2 = guess1.charAt(1);
+  document.getElementById("guess1--letter2").innerHTML = guess1Letter2;
   guess1Letter3 = guess1.charAt(2);
+  document.getElementById("guess1--letter3").innerHTML = guess1Letter3;
   guess1Letter4 = guess1.charAt(3);
+  document.getElementById("guess1--letter4").innerHTML = guess1Letter4;
   guess1Letter5 = guess1.charAt(4);
+  document.getElementById("guess1--letter5").innerHTML = guess1Letter5;
+
+  if (guess1Letter1 == currentWordLetter1) {
+    document.getElementById("guess1--letter1").style.background = "#538d4e";
+  } else if (currentWordleAnswer.includes(guess1Letter1)) {
+    document.getElementById("guess1--letter1").style.background = "#b59f3a";
+  } else {
+    document.getElementById("guess1--letter1").style.background = "black";
+  }
+  if (guess1Letter2 == currentWordLetter2) {
+    document.getElementById("guess1--letter2").style.background = "#538d4e";
+  } else if (currentWordleAnswer.includes(guess1Letter2)) {
+    document.getElementById("guess1--letter2").style.background = "#b59f3a";
+  } else {
+    document.getElementById("guess1--letter2").style.background = "black";
+  }
+  if (guess1Letter3 == currentWordLetter3) {
+    document.getElementById("guess1--letter3").style.background = "#538d4e";
+  } else if (currentWordleAnswer.includes(guess1Letter3)) {
+    document.getElementById("guess1--letter3").style.background = "#b59f3a";
+  } else {
+    document.getElementById("guess1--letter3").style.background = "black";
+  }
+  if (guess1Letter4 == currentWordLetter4) {
+    document.getElementById("guess1--letter4").style.background = "#538d4e";
+  } else if (currentWordleAnswer.includes(guess1Letter4)) {
+    document.getElementById("guess1--letter4").style.background = "#b59f3a";
+  } else {
+    document.getElementById("guess1--letter4").style.background = "black";
+  }
+  if (guess1Letter5 == currentWordLetter5) {
+    document.getElementById("guess1--letter5").style.background = "#538d4e";
+  } else if (currentWordleAnswer.includes(guess1Letter5)) {
+    document.getElementById("guess1--letter5").style.background = "#b59f3a";
+  } else {
+    document.getElementById("guess1--letter5").style.background = "black";
+  }
 }
